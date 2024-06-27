733f5c28fde97de2d53f42ff8d6b65f649be7f2e
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-06-28 00:17:44 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddToDo'
+import Todos from '../components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
